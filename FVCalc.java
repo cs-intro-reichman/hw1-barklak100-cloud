8693ceadf4341e86d89d425fee224df0efc318dc
bar.kalkstein <bar.kalkstein@post.runi.ac.il>
2025-11-06 17:41:12 +0200

    now

--- FVCalc.java
+++ FVCalc.java
@@ -3,19 +3,15 @@ public class FVCalc {
 	public static void main(String[] args)
 	{
 			// Replace this comment with your code
-	double currentValue;
-	double annualRatePercent;
-	int years;
-{
-double principal = Double.parseDouble(args[0]);
-double rate = Double.parseDouble(args[1]);
+ double principal = Double.parseDouble(args[0]);
+double annualRatePercent = Double.parseDouble(args[1]);
 int years = Integer.parseInt(args[2]);
-	double rate = annualRatePercent / 100.0;
-	double base = 1.0 + rate;
-	double power = Math.pow(base, years);
-	double futureValueDouble = currentValue * power;
-	int futureValueint = (int) futureValueDouble;
-	System.out.printf("after %d years' $%.0f saved at %.1f%% will yield $%d%n");
-}
+double rate = annualRatePercent / 100.0;
+double base = 1.0 + rate;
+ double power = Math.pow(base, years);
+ double futureValueDouble = principal * power;
+int futureValueint = (int) Math.round(futureValueDouble);
+System.out.printf("after %d years' $%.0f saved at %.1f%% will yield $%d%n",
+ years, principal, annualRatePercent, futureValueint);}
 	}
 }
